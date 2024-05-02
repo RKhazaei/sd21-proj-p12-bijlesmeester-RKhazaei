dc345d029c69bed6077a255b818eb7ad9d823868
RKhazaei <302769656@student.rocmondriaan.nl>
2024-05-02 15:39:59 +0200

    form aantal added

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -8,6 +8,9 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -42,14 +45,39 @@ class StudentController extends AbstractController
             'category' => $category,
         ]);
     }
+
     #[Route('/product/{id}', name: 'app_product')]
-    public function product(EntityManagerInterface $em, int $id): Response
+    public function makeOrder(EntityManagerInterface $em,Request $request, int $id): Response
     {
+        $product=$em->getRepository(Product::class)->find($id);
+        $form = $this->createFormBuilder()
+            ->add('amount', IntegerType::class, [
+                'required'=>true,
+                'data'=>1,
+                'label'=>'aantal'
+            ])
+            ->add('save', SubmitType::class)
+            ->getForm();
 
-        $product = $em->getRepository(Product::class)->find($id);
+        $session=$request->getSession();
+        $form->handleRequest($request);
+        if($form->isSubmitted() ) {
+            //als de variabele order in de sesion array niet bestaat maak deze aan
+            if(!$session->get('order')) {
+                $session->set('order',[]);
+            }
+            $amount=$form->get('amount')->getData();
+            //haal sessie op en voeg orderline toe
+            $order=$session->get('order');
+            $order[]=[$id,$amount];
+            $session->set('order',$order);
 
-        return $this->render('student/product.html.twig', [
-            'product' => $product,
+            $this->addFlash('success','product toegevoegd!');
+            return $this->redirectToRoute('app_order');
+        }
+        return $this->render('student/product.html.twig',[
+            'product'=>$product,
+            'form'=>$form
         ]);
     }
 
@@ -68,12 +96,13 @@ class StudentController extends AbstractController
     public function getProducts(EntityManagerInterface $em): Response
     {
         $products=$em->getRepository(Product::class)->findAll();
-        
+
 
         return $this->render('student/order.html.twig',[
             'products'=>$products
         ]);
     }
+
 
 
 }
